1fa98045d5c0fb8558ab12d5f9b7cc5c0a0159de
Mohammad Amaan Khan <2019bcs118@sggs.ac.in>
2022-09-24 22:46:21 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const ejs = require('ejs');
+
+dotenv.config({ path: "./config/config.env" });
+
+const routes = require("./routes/routes");
+
+const app = express();
+
+const publicPath = path.join(__dirname, "public");
+const viewPath = path.join(__dirname, 'views');
+app.use(express.static(publicPath));
+app.set('views', viewPath);
+
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+app.use(express.json());
+app.use(routes);
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    console.log(`https://localhost:${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
---
+++ routes/routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+
+const transport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        accessToken: process.env.PASSWORD
+    }
+});
+
+
+router.get("/", (req, res) => {
+    res.status(200).render("index.html");
+});
+
+
+router.get("/home", (req, res) => {
+    res.status(200).render("index.html");
+});
+
+
+router.post("/contact", (req, res) => {
+    message = {
+        from: `${req.body.email}`,
+        to: "amankhanak063@gmail.com",
+        subject: `Portfolio Message From - ${req.body.name}`,
+        text: `Message : ${req.body.message} \nfrom : ${req.body.email}`
+    };
+
+    // Sending the Mail
+    transport.sendMail(message, (err, info) => {
+        if (err) {
+            console.log("Failed to send Message.");
+        }
+    });
+
+    res.status(200).redirect("/");
+});
+
+
+module.exports = router;
